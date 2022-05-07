af82d372685e1e18b4f51ed3b5a775d90a0682fb
wendy.liu <aaa3520732@gmail.com>
2022-05-07 12:22:56 +0800

    feat: add veevalidate

--- src/components/CreditCardForm.vue
+++ src/components/CreditCardForm.vue
@@ -1,11 +1,58 @@
 <template>
-  <div class="form">I am form</div>
+  <Form v-slot="{ values }">
+    <p>Card Number</p>
+    <Field
+      name="CardNumber"
+      type="CardNumber"
+      :rules="isNumberRequired"
+      :value="cardNumber"
+      @input="$emit('update:cardNumber', $event.target.value)"
+    >
+    </Field>
+    <ErrorMessage name="CardNumber" />
+    <p>Card Holders</p>
+    <Field
+      name="CardHolders"
+      type="CardHolders"
+      :rules="isStringRequired"
+      :value="cardHolder"
+      @input="$emit('update:cardHolder', $event.target.value)"
+    ></Field>
+    <ErrorMessage name="CardHolders" />
+    <p></p>
+    {{ values }}
+  </Form>
 </template>
+
 <script>
+import { Form, Field, ErrorMessage } from "vee-validate";
 export default {
-  name: "Form",
+  name: "CreditCardForm",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  props: ["cardNumber", "cardHolder"],
+  emits: ["update:cardNumber", "update:cardHolder"],
   data() {
     return {};
+  },
+  methods: {
+    isNumberRequired(value) {
+      if (value && value.trim) {
+        return true;
+      } else {
+        return "請勿輸入空值";
+      }
+    },
+    isStringRequired(value) {
+      if (value && value.trim) {
+        return true;
+      } else {
+        return "請勿輸入空值";
+      }
+    },
   },
 };
 </script>
