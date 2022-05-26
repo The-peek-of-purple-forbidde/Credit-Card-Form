d4bdb8e50d2ba393b30158d6361e05679a8c530f
Andy Tseng <zatchbell.1206@gmail.com>
2022-05-26 14:45:05 +0800

    Revert "Validation (#16)"
    This reverts commit 0b8befb3ee09b76d4da003934de394dc27624114.

--- src/assets/validation/cardHolder.js
+++
@@ -1,4 +0,0 @@
-//* 判別是否為英文名字 1.需為英文名字 2.開頭結尾不得為空白 3.中間可有空白
-export function isFormattedEnglishName(value) {
-  return /^[a-zA-Z][a-zA-Z ]*[a-zA-Z]+$/.test(value) === true;
-}
--- src/assets/validation/isNoEarlierThanThisYear.js
+++
@@ -1,5 +0,0 @@
-function isNoEarlierThanThisYear(value) {
-  return value >= new Date().getFullYear() === true;
-}
-
-export { isNoEarlierThanThisYear };
