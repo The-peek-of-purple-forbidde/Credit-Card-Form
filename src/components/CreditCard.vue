4bcb56b32dd5686d7572dc6a896cfae36ac8f688
AJay <55973225+a7773077567@users.noreply.github.com>
2022-05-26 12:22:52 +0800

    feat: Creadit-card-front layout done (#14)
    gpgp

--- src/components/CreditCard.vue
+++ src/components/CreditCard.vue
@@ -6,7 +6,53 @@
   <div class="credit-card">edit line -----------------------</div>
   <div class="scene scene--card">
     <div class="card">
-      <div class="card__face card__face--front">正面待補</div>
+      <!-- 卡片前面 (AJay) -->
+      <div class="card__face card__face--front">
+        <div class="card-front">
+          <div class="card-front__wrapper">
+            <div class="card-front__header">
+              <img
+                class="card-front__chip"
+                src="@/assets/images/chip.png"
+                alt="card chip"
+              />
+              <img
+                class="card-front__type"
+                src="@/assets/images/visa.png"
+                alt="visa"
+              />
+            </div>
+            <div class="card-front__body">
+              <label for="cardNumber" class="card-front__number">
+                <span>####</span>
+                <span>####</span>
+                <span>####</span>
+                <span>####</span>
+              </label>
+            </div>
+            <div class="card-front__footer">
+              <label class="card-front__info" for="cardName">
+                <div class="card-front__holder">Card Holder</div>
+                <div class="card-front__name">FULL NAME</div>
+              </label>
+              <div class="card-front__date">
+                <label class="card-front__date-title" for="cardMonth"
+                  >Expires</label
+                >
+                <div class="card-front__date-body">
+                  <label class="card-front__date-item" for="cardMonth"
+                    >MM</label
+                  >
+                  <span class="card-front__date-item">/</span>
+                  <label class="card-front__date-item" for="cardYear">YY</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 卡片後面 (JiaXian) -->
       <div class="card__face card__face--back">
         <div class="card-back__band"></div>
         <div class="card-back__content">
@@ -73,13 +119,13 @@ export default {
 // 正面的卡片樣式
 .card__face--front {
   box-shadow: 0px 12px 40px -12px #132b5a;
-  background-image: url(https://github.com/muhammed/vue-interactive-paycard/blob/master/src/assets/images/19.jpeg?raw=true);
+  background-image: url("@/assets/images/19.jpeg");
 }
 
 // 背面的卡片樣式
 .card__face--back {
   box-shadow: 0px 12px 40px -12px #132b5a;
-  background-image: url(https://github.com/muhammed/vue-interactive-paycard/blob/master/src/assets/images/19.jpeg?raw=true);
+  background-image: url("@/assets/images/19.jpeg");
 
   transform: rotateY(180deg);
 }
@@ -118,6 +164,81 @@ export default {
   height: 40px;
   img {
     width: 100%;
+  }
+}
+
+// 前面部分 (AJay)
+
+.card-front {
+  height: 100%;
+  > * {
+    box-sizing: border-box;
+  }
+  &__wrapper {
+    height: 100%;
+    padding: 25px 15px;
+  }
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    margin-bottom: 40px;
+    padding: 0 10px;
+  }
+  &__chip {
+    width: 60px;
+  }
+  &__type {
+    height: 45px;
+    width: auto;
+    max-width: 100px;
+  }
+  &__body {
+    margin-bottom: 35px;
+  }
+  &__number {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1;
+    padding: 10px 15px;
+    span {
+      font-size: 26px;
+    }
+  }
+  &__footer {
+    display: flex;
+    justify-content: space-between;
+    font-size: 0;
+  }
+  &__info {
+    padding: 10px 15px;
+  }
+  &__holder {
+    margin-bottom: 6px;
+    text-align: left;
+    font-size: 13px;
+    opacity: 0.7;
+  }
+  &__name {
+    font-size: 18px;
+    line-height: 1;
+  }
+  &__date {
+    padding: 10px;
+  }
+  &__date-title {
+    display: block;
+    width: 100%;
+    margin-bottom: 6px;
+    text-align: left;
+    font-size: 13px;
+    opacity: 0.7;
+  }
+  &__date-body {
+    line-height: 1;
+  }
+  &__date-item {
+    font-size: 18px;
   }
 }
 </style>
